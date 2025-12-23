b3115767046a6452683cd61d9ebc0ab356cdec86
richstoremipad <richstoremipad@gmail.com>
2025-12-24 03:54:34 +0700

    Update main.go

--- main.go
+++ main.go
@@ -1,37 +1,15 @@
 package main
 
 import (
-	"golang.org/x/mobile/app"
-	"golang.org/x/mobile/event/lifecycle"
-	"golang.org/x/mobile/event/paint"
-	"golang.org/x/mobile/event/size"
-	"golang.org/x/mobile/gl"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
-	app.Main(func(a app.App) {
-		var glctx gl.Context
-		for e := range a.Events() {
-			switch e := a.Filter(e).(type) {
-			case lifecycle.Event:
-				switch e.Crosses(lifecycle.StageVisible) {
-				case lifecycle.CrossOn:
-					glctx, _ = e.DrawContext.(gl.Context)
-				case lifecycle.CrossOff:
-					glctx = nil
-				}
-			case size.Event:
-				// Menangani perubahan ukuran layar
-			case paint.Event:
-				if glctx == nil || e.External {
-					continue
-				}
-				// Mewarnai layar menjadi biru (RGB)
-				glctx.ClearColor(0, 0, 1, 1)
-				glctx.Clear(gl.COLOR_BUFFER_BIT)
-				a.Publish()
-				a.Send(paint.Event{}) // Terus menggambar
-			}
-		}
-	})
+	myApp := app.New()
+	myWindow := myApp.NewWindow("Aplikasi Saya")
+
+	myWindow.SetContent(widget.NewLabel("Halo! Ini aplikasi berukuran lebih kecil."))
+
+	myWindow.ShowAndRun()
 }
