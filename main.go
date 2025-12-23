204c3acbb5be149f48d805916c2bedeab51008af
richstoremipad <richstoremipad@gmail.com>
2025-12-24 03:41:57 +0700

    Update main.go

--- main.go
+++ main.go
@@ -1,14 +1,37 @@
 package main
 
 import (
-	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/widget"
+	"golang.org/x/mobile/app"
+	"golang.org/x/mobile/event/lifecycle"
+	"golang.org/x/mobile/event/paint"
+	"golang.org/x/mobile/event/size"
+	"golang.org/x/mobile/gl"
 )
 
 func main() {
-	myApp := app.New()
-	myWindow := myApp.NewWindow("Hello Fyne")
-
-	myWindow.SetContent(widget.NewLabel("Halo dari Go + Fyne!"))
-	myWindow.ShowAndRun()
+	app.Main(func(a app.App) {
+		var glctx gl.Context
+		for e := range a.Events() {
+			switch e := a.Filter(e).(type) {
+			case lifecycle.Event:
+				switch e.Crosses(lifecycle.StageVisible) {
+				case lifecycle.CrossOn:
+					glctx, _ = e.DrawContext.(gl.Context)
+				case lifecycle.CrossOff:
+					glctx = nil
+				}
+			case size.Event:
+				// Menangani perubahan ukuran layar
+			case paint.Event:
+				if glctx == nil || e.External {
+					continue
+				}
+				// Mewarnai layar menjadi biru (RGB)
+				glctx.ClearColor(0, 0, 1, 1)
+				glctx.Clear(gl.COLOR_BUFFER_BIT)
+				a.Publish()
+				a.Send(paint.Event{}) // Terus menggambar
+			}
+		}
+	})
 }
